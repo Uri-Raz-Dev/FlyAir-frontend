0e3c5545aceac0aabe51a1edfb1621cbe938da85
Idan Elfasi <idan60167@gmail.com>
2024-08-20 19:16:42 +0300

    app header starting building

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router'
 import { useSelector } from 'react-redux'
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 import { logout } from '../store/actions/user.actions'
+import { StayFilter } from './StayFilter.jsx'
 
 export function AppHeader() {
 	const user = useSelector(storeState => storeState.userModule.user)
@@ -20,28 +21,12 @@ export function AppHeader() {
 
 	return (
 		<header className="app-header full">
-			<nav>
-				<NavLink to="/" className="logo">
-					E2E Demo
-				</NavLink>
-				<NavLink to="about">About</NavLink>
-				<NavLink to="stay">Stays</NavLink>
-				<NavLink to="chat">Chat</NavLink>
-				<NavLink to="review">Review</NavLink>
-
-				{user?.isAdmin && <NavLink to="/admin">Admin</NavLink>}
-
-				{!user && <NavLink to="login" className="login-link">Login</NavLink>}
-				{user && (
-					<div className="user-info">
-						<Link to={`user/${user._id}`}>
-							{/* {user.imgUrl && <img src={user.imgUrl} />} */}
-							{user.fullname}
-						</Link>
-						{/* <span className="score">{user.score?.toLocaleString()}</span> */}
-						<button onClick={onLogout}>logout</button>
-					</div>
-				)}
+			<nav className='flex'>
+				<Link to={"/"} >FlyAir</Link>
+				<StayFilter />
+				<Link to={"/"}>switch to host </Link>  {/* //new user will show "airbnb your home" */}
+				<button className='lang'>lang-list</button>
+				<button className='user-app-header'>user</button>
 			</nav>
 		</header>
 	)
