2a702b876064aa9929551d562fe2b9f5c5ac742e
Uri-Raz-Dev <147313459+Uri-Raz-Dev@users.noreply.github.com>
2024-08-23 21:01:09 +0300

    Add StayDetails header and img list

--- src/pages/StayDetails.jsx
+++ src/pages/StayDetails.jsx
@@ -5,12 +5,17 @@ import { Link } from 'react-router-dom'
 
 import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service'
 import { loadStay, addStayMsg } from '../store/actions/stay.actions'
+import { SvgIcon } from '../cmps/Svgicon'
 
 
 export function StayDetails() {
 
   const { stayId } = useParams()
   const stay = useSelector(storeState => storeState.stayModule.stay)
+  const { _id, name, type, imgurls, price, capacity } = stay || {}
+  const { city, country, countryCode, address, lat, lag } = stay?.location || {}
+  console.log(stay);
+
 
   useEffect(() => {
     loadStay(stayId)
@@ -28,16 +33,54 @@ export function StayDetails() {
 
   return (
     <section className="stay-details">
-      <Link to="/stay">Back to list</Link>
-      <h1>Stay Details</h1>
-      {stay && <div>
-        <h3>{stay.vendor}</h3>
-        <h4>${stay.price}</h4>
-        <pre> {JSON.stringify(stay, null, 2)} </pre>
-      </div>
-      }
-      <button onClick={() => { onAddStayMsg(stay._id) }}>Add stay msg</button>
+      <header className="details-header">
+        <h1>{name}</h1>
+        <div className="add-to-favorites">
+          <div className="icon-wrapper">
+            <span><SvgIcon iconName={"share"}></SvgIcon></span>
+            <span>Share</span>
+          </div>
+          <div className="icon-wrapper">
+            <span><SvgIcon iconName={"save"}></SvgIcon></span>
+            <span>Save</span>
+          </div>
+        </div>
+      </header>
 
+
+      <section className="details-image-list">
+
+        <ul className="details-image-grid">
+
+          {
+            imgurls && imgurls.length > 0
+              ? imgurls.map((img, index) => (
+                <li key={index}><img src={img} alt="stay image" /></li>
+
+
+              ))
+              : null
+          }
+
+        </ul>
+
+      </section>
+
+      <section className='stay-info'>
+        <header className="info-header">
+          <h2></h2>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <div className="review-info">
+            <Link to="review"></Link>
+
+          </div>
+        </header>
+      </section>
     </section>
   )
 }
--- src/services/svg.service.js
+++ src/services/svg.service.js
@@ -21,6 +21,14 @@ const SVGS = {
 
     search: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" style="display:block;fill:none;height:16px;width:16px;stroke:currentColor;stroke-width:4;overflow:visible" aria-hidden="true" role="presentation" focusable="false"><path fill="none" d="M13 24a11 11 0 1 0 0-22 11 11 0 0 0 0 22zm8-3 9 9"></path></svg>`,
 
+    //stay details
+
+    share: `<svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 16px; width: 16px; stroke: currentcolor; stroke-width: 2; overflow: visible;"><path d="m27 18v9c0 1.1046-.8954 2-2 2h-18c-1.10457 0-2-.8954-2-2v-9m11-15v21m-10-11 9.2929-9.29289c.3905-.39053 1.0237-.39053 1.4142 0l9.2929 9.29289" fill="none"></path></svg>`,
+
+    save: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 16px; width: 16px; stroke: currentcolor; stroke-width: 2; overflow: visible;"><path d="M16 28c7-4.73 14-10 14-17a6.98 6.98 0 0 0-7-7c-1.8 0-3.58.68-4.95 2.05L16 8.1l-2.05-2.05a6.98 6.98 0 0 0-9.9 0A6.98 6.98 0 0 0 2 11c0 7 7 12.27 14 17z"></path></svg>`,
+
+    reviewstar: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 16px; width: 16px; fill: currentcolor;"><path fill-rule="evenodd" d="m15.1 1.58-4.13 8.88-9.86 1.27a1 1 0 0 0-.54 1.74l7.3 6.57-1.97 9.85a1 1 0 0 0 1.48 1.06l8.62-5 8.63 5a1 1 0 0 0 1.48-1.06l-1.97-9.85 7.3-6.57a1 1 0 0 0-.55-1.73l-9.86-1.28-4.12-8.88a1 1 0 0 0-1.82 0z"></path></svg>`,
+
     //img icons
 
     imgshare: `<svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 14px; width: 14px; stroke: currentcolor; stroke-width: 3.42857; overflow: visible;"><path d="m27 18v9c0 1.1046-.8954 2-2 2h-18c-1.10457 0-2-.8954-2-2v-9m11-15v21m-10-11 9.2929-9.29289c.3905-.39053 1.0237-.39053 1.4142 0l9.2929 9.29289" fill="none"></path></svg>`,
