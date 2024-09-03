c18a488131982d546ef7e4552af1f507e084d58d
Teperr <omerteperr@gmail.com>
2024-09-03 10:48:06 +0300

    Add nav user

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -4,11 +4,27 @@ import { NavLink } from 'react-router-dom'
 export function LoginSignup() {
     return (
         <div className="login-page">
-            <nav>
-                <NavLink to=".">Login</NavLink>
-                <NavLink to="signup">Signup</NavLink>
+            <nav className="login-nav">
+                <a><NavLink to=".">Login</NavLink></a>
+                <a className='Signup-link'><NavLink to="signup">Signup</NavLink></a>
+                <a>Gift cards</a>
+                <a>Airbnb your home</a>
+                <a>Help center</a>
+               
+
+
+
             </nav>
-            <Outlet/>
+            {/* <Outlet/> */}
         </div>
     )
 }
+
+{/* <div className="login-page">
+<nav>
+    <NavLink to=".">Login</NavLink>
+    <NavLink to="signup">Signup</NavLink>
+    <p>ddd</p>
+</nav>
+<Outlet/>
+</div> */}
