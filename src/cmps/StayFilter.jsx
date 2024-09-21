5b1f1eb93d66558dd29ce0bafc289eddbdf0ddcd
Idan Elfasi <idan60167@gmail.com>
2024-09-21 17:06:21 +0300

    filter fix

--- src/cmps/StayFilter.jsx
+++ src/cmps/StayFilter.jsx
@@ -10,34 +10,35 @@ import DateRangePickerComponent from './DateRangePickerComponent.jsx';
 import { Guests } from './Guests.jsx';
 import { CalendarPicker } from './CalendarPicker.jsx';
 import { GuestsModal } from './GuestsModal.jsx';
+import { selectClasses } from '@mui/material';
 // import '../assets/styles/basics/_helpers.scss';
 
 export function StayFilter({ filterBy, onSetFilter }) {
-    const [selectedRegion, setSelectedRegion] = useState('');
-
-    const [selectedCheckIn, setSelectedCheckIn] = useState(null);
-    const [selectedCheckOut, setSelectedCheckOut] = useState(null);
+    const [selectedRegion, setSelectedRegion] = useState('')
+    const [selectedCheckIn, setSelectedCheckIn] = useState('')
+    const [selectedCheckOut, setSelectedCheckOut] = useState('')
+    const [selectedCapacity, setSelectedCapacity] = useState(0)
+    // const [guestsTree, setGuestTree] = useState({
+    //     adults: 0,
+    //     children: 0,
+    //     infants: 0,
+    //     pets: 0,
+    // })
 
     const [checkInFilter, setCheckInFilter] = useState(false)
     const [checkOutFilter, setCheckOutFilter] = useState(false)
-    const [adultsAmount, setAdultsAmount] = useState(0);
-    const [childrenAmount, setChildrenAmount] = useState(0);
-    const [infantsAmount, setInfantsAmount] = useState(0);
-    const [petsAmount, setPetsAmount] = useState(0);
 
-    const [guestsTree, setGuestTree] = useState({
-        adults: 0,
-        children: 0,
-        infants: 0,
-        pets: 0,
-    })
-
-    // const [selectedGuests, setSelectedGuests] = useState(0);
+    const [adultsAmount, setAdultsAmount] = useState(0)
+    const [childrenAmount, setChildrenAmount] = useState(0)
+    const [infantsAmount, setInfantsAmount] = useState(0)
+    const [petsAmount, setPetsAmount] = useState(0)
 
 
-    const [isRegionPickerOpen, setRegionPickerOpen] = useState(false);
-    const [isDatesPickerOpen, setDatesPickerOpen] = useState(false);
-    const [isGuestsOpen, setGuestsOpen] = useState(false);
+
+
+    const [isRegionPickerOpen, setRegionPickerOpen] = useState(false)
+    const [isDatesPickerOpen, setDatesPickerOpen] = useState(false)
+    const [isGuestsOpen, setGuestsOpen] = useState(false)
 
 
 
@@ -53,22 +54,9 @@ export function StayFilter({ filterBy, onSetFilter }) {
 
     const [filterToEdit, setFilterToEdit] = useState({ ...filterBy })
 
-    // const [checkInDate, setCheckInDate] = useState(null);
-    // const [checkOutDate, setCheckOutDate] = useState(null);
 
-    // const handleDateChange = (checkIn, checkOut) => {
-    //     setCheckInDate(checkIn);
-    //     setCheckOutDate(checkOut);
 
-    //     // Close the modal if both dates are selected
-    //     if (checkIn && checkOut) {
-    //         setIsModalOpen(false);
-    //     }
-    // };
 
-    useEffect(() => {
-        onSetFilter(filterToEdit)
-    }, [filterToEdit])
 
 
     function handleChange(ev) {
@@ -90,24 +78,15 @@ export function StayFilter({ filterBy, onSetFilter }) {
         }
         setFilterToEdit({ ...filterToEdit, [field]: value })
         console.log('filterToEdit:', filterToEdit)
+
     }
+
     function handleFocus(inputRef) {
         if (inputRef.current) {
             inputRef.current.focus();
         }
 
     }
-
-    // function clearFilter() {
-    //     setFilterToEdit({ ...filterToEdit, txt: '', minSpeed: '', maxPrice: '' })
-    // }
-
-    // function clearSort() {
-    //     setFilterToEdit({ ...filterToEdit, sortField: '', sortDir: '' })
-    // }
-
-    // const [showModal, setShowModal] = useState(false);
-
 
 
 
@@ -173,85 +152,66 @@ export function StayFilter({ filterBy, onSetFilter }) {
 
         if (region === selectedRegion || region === "flexibile") {
             setSelectedRegion('')
-            setFilterToEdit({ ...filterToEdit, region: '' })
+            // setFilterToEdit({ ...filterToEdit, region: '' })
+
         }
         else {
             setSelectedRegion(region)
-            setFilterToEdit({ ...filterToEdit, region })
+            // setFilterToEdit({ ...filterToEdit, region })
         }
         setRegionPickerOpen(false)
+        setDatesPickerOpen(true)
+        handleFocus(checkIninputRef)
     }
 
-    function handleGuestsTree(adults, children, infants, pets, type, operation) {
-        var dec = 1
-        if (type === 'adults') {
-            if (operation === 'increment')  dec = 1
-            else dec = -1
+    // function handleGuestsTree(adults, children, infants, pets, type, operation) {
+    //     var dec = 1
+    //     if (type === 'adults') {
+    //         if (operation === 'increment') dec = 1
+    //         else dec = -1
 
-            setGuestTree({ ...guestsTree, adults: adults + dec, children: children, infants: infants, pets: pets })
-            setFilterToEdit({
-                ...filterToEdit,
-                guests: {
-                  ...filterToEdit.guests, // Spread the previous guests object
-                  adults: guestsTree.adults+dec, // Update values
-                  children: guestsTree.children,
-                  infants: guestsTree.infants,
-                  pets: guestsTree.pets
-                }
-              });
-    
-        }
-        else if (type === 'children') {
-            if (operation === 'increment') dec = 1
-            else dec = -1
-            setGuestTree({ ...guestsTree, adults: adults, children: children + dec, infants: infants, pets: pets })
-            setFilterToEdit({
-                ...filterToEdit,
-                guests: {
-                  ...filterToEdit.guests, // Spread the previous guests object
-                  adults: guestsTree.adults, // Update values
-                  children: guestsTree.children+dec,
-                  infants: guestsTree.infants,
-                  pets: guestsTree.pets
-                }
-              });
-    
-        }
-        else if (type === 'infants') {
-            if (operation === 'increment') dec = 1
-            else dec = -1
+    //         setGuestTree({ ...guestsTree, adults: adults + dec, children: children, infants: infants, pets: pets })
 
-            setGuestTree({ ...guestsTree, adults: adults, children: children, infants: infants + dec, pets: pets })
-            setFilterToEdit({
-                ...filterToEdit,
-                guests: {
-                  ...filterToEdit.guests, // Spread the previous guests object
-                  adults: guestsTree.adults, // Update values
-                  children: guestsTree.children,
-                  infants: guestsTree.infants+dec,
-                  pets: guestsTree.pets
-                }
-              });
-    
-        }
-        else if (type === 'pets') {
-            if (operation === 'increment')   dec = 1
-            else dec = -1
-            setGuestTree({ ...guestsTree, adults: adults, children: children, infants: infants, pets: pets + dec })
-            setFilterToEdit({
-                ...filterToEdit,
-                guests: {
-                  ...filterToEdit.guests, // Spread the previous guests object
-                  adults: guestsTree.adults, // Update values
-                  children: guestsTree.children,
-                  infants: guestsTree.infants,
-                  pets: guestsTree.pets+dec
-                }
-              });
-    
-        }
-    }
+
+    //     }
+    //     else if (type === 'children') {
+    //         if (operation === 'increment') dec = 1
+    //         else dec = -1
+    //         setGuestTree({ ...guestsTree, adults: adults, children: children + dec, infants: infants, pets: pets })
+
+
+    //     }
+    //     else if (type === 'infants') {
+    //         if (operation === 'increment') dec = 1
+    //         else dec = -1
+
+    //         setGuestTree({ ...guestsTree, adults: adults, children: children, infants: infants + dec, pets: pets })
+
+
+    //     }
+    //     else if (type === 'pets') {
+    //         if (operation === 'increment') dec = 1
+    //         else dec = -1
+    //         setGuestTree({ ...guestsTree, adults: adults, children: children, infants: infants, pets: pets + dec })
+
+
+    //     }
+
+    //     setFilterToEdit({
+    //         ...filterToEdit,
+    //         guests: {
+    //             ...filterToEdit.guests, // Spread the previous guests object
+    //             adults: guestsTree.adults, // Update values
+    //             children: guestsTree.children,
+    //             infants: guestsTree.infants,
+    //             pets: guestsTree.pets
+    //         }
+    //     })
+
+    // }
+
     function handleAmountChange(type, operation) {
+       
         switch (type) {
             case "adults":
                 setAdultsAmount((prevAmount) =>
@@ -290,76 +250,40 @@ export function StayFilter({ filterBy, onSetFilter }) {
                 );
                 break;
             default:
-                setFilterToEdit({
-                    ...filterToEdit,
-                    guests: {
-                      ...filterToEdit.guests, // Spread the previous guests object
-                      adults: guestsTree.adults, // Update values
-                      children: guestsTree.children,
-                      infants: guestsTree.infants,
-                      pets: guestsTree.pets
-                    }
-                  });
+                // handleGuestsTree(adultsAmount, childrenAmount, infantsAmount, petsAmount, type, operation)
                 break;
         }
 
-        handleGuestsTree(adultsAmount, childrenAmount, infantsAmount, petsAmount, type, operation)
+        setSelectedCapacity((prevAmount) => (operation === 'increment') ? prevAmount + 1 : prevAmount - 1)
 
-
-
-        // if (type === 'adults') {
-        //     setGuestTree((prevguestsTree) =>
-        //         prevguestsTree.adults + 1
-        //     )
-
-        // }
-
-        // if (type === 'children') {
-        //     setGuestTree({ ...guestsTree, children: childrenAmount })
-
-        // }
-
-        // if (type === 'infants') {
-        //     setGuestTree({ ...guestsTree, infants: infantsAmount })
-
-        // }
-
-        // if (type === 'pets') {
-        //     setGuestTree({ ...guestsTree, pets: petsAmount })
-
-        // }
-        // setSelectedGuests((prev) => {
-        //     if (operation === "increment") {
-        //         return prev + 1;
-        //     } else {
-        //         return prev > 0 ? prev - 1 : 0;
-        //     }
-        // });
-    }
-
-    function showGuestsValue(guestsTree) {
-
-        const adults = guestsTree.adults > 0 ? guestsTree.adults : 0
-        const children = guestsTree.children > 0 ? guestsTree.children : 0
-        const adultsChildrenInt = adults + children
-        const adultsAndChildren = (adultsChildrenInt) === 0 ? '': `${adultsChildrenInt} guests`
-        // const adultsAndChildren =  (``+(guestsTree.adults + guestsTree.children)) || ``
-        // const guests = adultsAndChildren? ` ${adultsAndChildren} guests ` :``
-        const infants = guestsTree.infants ? `${ guestsTree.infants} infants` : ''
-        const pets = guestsTree.pets ? `${ guestsTree.pets} pets` : ''
-        const str = `${adultsAndChildren} ${infants} ${pets}`
-        console.log(str);
-
-        return (
-
-            // ` ${guests} ${infants} ${pets}  `
-            str
-
-        )
+        // setFilterToEdit({ ...filterToEdit, capacity: operation = 'increment' ? prevAmount + 1 : prevAmount - 1 })
 
     }
+
+    // function showGuestsValue(guestsTree) {
+
+    //     const adults = guestsTree.adults > 0 ? guestsTree.adults : 0
+    //     const children = guestsTree.children > 0 ? guestsTree.children : 0
+
+    //     const adultsChildrenInt = adults + children
+    //     const adultsAndChildren = (adultsChildrenInt) === 0 ? '' : `${adultsChildrenInt} guests`
+
+    //     const infants = guestsTree.infants ? `${guestsTree.infants} infants` : ''
+    //     const pets = guestsTree.pets ? `${guestsTree.pets} pets` : ''
+
+    //     const str = `${adultsAndChildren} ${infants} ${pets}`
+    //     console.log(str);
+
+    //     return (
+
+    //         str
+
+    //     )
+
+    // }
 
     function displayDateShortly(date) {
+        if (!date) return ''
         const shortMonthName = new Date(date).toLocaleString('en-US', { month: 'short' });
         const shortDate = `${new Date(date).getDate()} ${shortMonthName}`
         return shortDate
@@ -368,62 +292,44 @@ export function StayFilter({ filterBy, onSetFilter }) {
         console.log(`check in date : ${checkIn}`);
 
         setSelectedCheckIn(checkIn)
-        setFilterToEdit({ ...filterToEdit, startDate: checkIn })
-        // setCheckOutFilter(true)
-        // handleFocus(checkOutinputRef)
-        // handleFocus(checkOutinputRef)
+        // setFilterToEdit({ ...filterToEdit, startDate: checkIn })
+
+
     }
     function handleCheckOut(checkOut) {
         console.log(`check out date : ${checkOut}`);
 
         setSelectedCheckOut(checkOut)
-        setFilterToEdit({ ...filterToEdit, endDate: checkOut })
-
+        // setFilterToEdit({ ...filterToEdit, endDate: checkOut })
     }
 
     function clearDates() {
         setSelectedCheckIn(null)
         setSelectedCheckOut(null)
         setDatesPickerOpen(false)
-        setFilterToEdit({ ...filterToEdit, startDate: null, endDate: null })
     }
 
-    function updateGuestesFilter(){
 
+    function handleFilter(ev) {
+        ev.preventDefault()
 
-        setFilterToEdit({
-            ...filterToEdit,
-            guests: {
-              ...filterToEdit.guests, // Spread the previous guests object
-              adults: guestsTree.adults, // Update values
-              children: guestsTree.children,
-              infants: guestsTree.infants,
-              pets: guestsTree.pets
-            }
-          });
+        setFilterToEdit({ ...filterToEdit, region: selectedRegion, startDate: selectedCheckIn, endDate: selectedCheckOut, capacity: selectedCapacity })
+        onSetFilter(filterToEdit)
 
-        // setFilterToEdit({
-        //             ...filterToEdit, guests: {
-        //                 ...filterToEdit.guests, adults:adultsAmount
-        //                 , children: childrenAmount, infants:infantsAmount, pets: petsAmount,
-        //             }
-        //         })
     }
 
-    // console.log({selectedGuests});
-    console.log(guestsTree);
-    console.log(filterToEdit);
+
+    console.log(filterToEdit)
 
     return (
 
-        // <div className={isFilterOpen ? "search-filter" : "search-filter active"}>
-        <div className='search-filter'>
+        <form onSubmit={handleFilter} className='search-filter'>
 
             <a onClick={openRegionsModal} className={`search-filter-item ${regionActive.current} `}>
                 <div>
                     <label>Where</label>
-                    <input ref={regioninputRef} onChange={handleChange} type="text" value={selectedRegion}
-                        placeholder="Search destinations" name='region' />
+                    <input ref={regioninputRef} onChange={handleChange} type="text" value={selectedRegion || ''}
+                        placeholder="Search destinations" autoComplete="off" name='region' />
 
                 </div>
             </a>
@@ -432,7 +338,8 @@ export function StayFilter({ filterBy, onSetFilter }) {
                 <div>
 
                     <label>Check in</label>
-                    <input ref={checkIninputRef} value={selectedCheckIn ? displayDateShortly(selectedCheckIn) : null}
+                    <input ref={checkIninputRef} onChange={handleChange} value={displayDateShortly(selectedCheckIn)}
+                        autoComplete="off"
                         className='check-in' name='startDate' type="text" placeholder="Add dates" />
                     {/* <button style={{display:'none'}} onClick={clearDates}>x</button> */}
                 </div>
@@ -443,24 +350,27 @@ export function StayFilter({ filterBy, onSetFilter }) {
                 <div>
 
                     <label>Check out</label>
-                    <input ref={checkOutinputRef} value={selectedCheckOut ? displayDateShortly(selectedCheckOut) : null} name='endDate'
-                        className={`check-in`} type="text" placeholder="Add dates" />
+                    <input ref={checkOutinputRef} onChange={handleChange} value={displayDateShortly(selectedCheckOut)}
+                        name='endDate'
+                        className='check-in' type="text" autoComplete="off" placeholder="Add dates" />
                     {/* <button style={{display:'none'}} onClick={clearDates}>x</button> */}
                 </div>
             </a>
 
 
-            <a onClick={openGuestsModal} href='#' className="search-filter-item">
+            <a onClick={openGuestsModal} className="search-filter-item">
                 <div>
 
                     <label>Who</label>
-                    {/* <input ref={guestsinputRef} value={`${selectedGuests } guests`} type="text" placeholder="Add guests" /> */}
-                    <input ref={guestsinputRef} value={showGuestsValue(guestsTree)} type="text" placeholder="Add guests" />
+                    <input ref={guestsinputRef}
+                        onChange={handleChange}
+                        value={selectedCapacity>0 ? `${selectedCapacity} Guests` : ''}
+                        type="text" placeholder="Add guests" autoComplete="off" name='capacity' />
                 </div>
             </a>
 
             <div className='search-btn-container'>
-                <button className="search-button">
+                <button type="submit" className="search-button">
                     <span><SvgIcon iconName="search" /></span>
                 </button>
             </div>
@@ -472,29 +382,31 @@ export function StayFilter({ filterBy, onSetFilter }) {
             </Modal>
 
             <Modal show={isDatesPickerOpen} onClose={handledatesCloseModal}>
-                {/* <DatePickerr handleCheckIn={handleCheckIn} handleCheckOut={handleCheckOut} isCheckIn={checkInFilter.current} isCheckOut={checkOutFilter.current} /> */}
-                {/* <CalendarPicker/> */}
-                <DateRange handleCheckIn={handleCheckIn} handleCheckOut={handleCheckOut} isCheckIn={checkInFilter} isCheckOut={checkOutFilter} />
+
+                <DateRange handleCheckIn={handleCheckIn} handleCheckOut={handleCheckOut}
+                    isCheckIn={checkInFilter} isCheckOut={checkOutFilter}
+                    checkIn={selectedCheckIn} checkOut={selectedCheckOut} />
             </Modal>
 
             <Modal show={isGuestsOpen} onClose={handleCloseGuests}>
-                {/* <Guests handleGuestsTree={handleGuestsTree} /> */}
-                <GuestsModal adultsAmount={adultsAmount}
+
+                <GuestsModal
+                    adultsAmount={adultsAmount}
                     childrenAmount={childrenAmount}
                     infantsAmount={infantsAmount}
                     petsAmount={petsAmount}
                     handleAmountChange={handleAmountChange}
-                    // onSetFilter={setFilterToEdit}
-                    updateGuestesFilter={updateGuestesFilter}
-                    />
-            
+
+                />
+
             </Modal>
 
 
+        </form>
 
 
 
-        </div>
+        // </div>
 
     )
 
